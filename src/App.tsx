d6cbd176b075f329e74ca8736772367699653d62
huangchengwen <huangchengwen@hetao101.com>
2022-04-01 14:21:28 +0800

    xml format

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useRef } from "react";
+import { Tabs, Button, Upload } from "antd";
 import Blockly from "blockly";
 import "./libs/customBlocks/custom_Blocks";
 import "blockly/python";
 import "blockly/javascript";
 import "./libs/customBlocks/codeGenerator";
 import { BlocklyWorkspace } from "react-blockly";
+import xmlFormatter from "xml-formatter";
 import {
     INITIAL_XML,
     INITIAL_TOOLBOX_JSON,
     MEDIA_PATH,
 } from "./libs/customBlocks/initContent";
+
+import { savaProject, loadProjectXmlAsync } from "./libs/helpers/project";
+import "antd/dist/antd.css";
 import "./App.css";
+
+const { TabPane } = Tabs;
 
 function App() {
     const [xml, setXml] = useState("");
     const [javascriptCode, setJavascriptCode] = useState("");
+    const workspaceInstance = useRef(null);
 
     const workspaceDidChange = useCallback((workspace: any) => {
         try {
@@ -25,39 +33,73 @@ function App() {
         }
     }, []);
 
+    const selectTabHandle = useCallback((key: string) => {
+        console.log(key);
+    }, []);
+
+    const saveProjectClick = useCallback(() => {
+        savaProject(xml, javascriptCode);
+    }, [xml, javascriptCode]);
+
+    const loadProjectHandle = useCallback(async (content: Blob) => {
+        const projectXml = await loadProjectXmlAsync(content);
+        console.log(workspaceInstance.current);
+    }, []);
+
+    const injectHandle = useCallback((workspace: any) => {
+        workspaceInstance.current = workspace;
+    }, []);
+
+    const xmlChangeHandle = (str: string) => {
+        setXml(xmlFormatter(str));
+    };
+
     return (
-        <div className="App">
-            <BlocklyWorkspace
-                toolboxConfiguration={INITIAL_TOOLBOX_JSON}
-                initialXml={INITIAL_XML}
-                className="fill-height"
-                workspaceConfiguration={{
-                    grid: {
-                        spacing: 20,
-                        length: 3,
-                        colour: "#ccc",
-                        snap: true,
-                    },
-                    zoom: {
-                        controls: true,
-                        wheel: true,
-                        startScale: 1.0,
-                        maxScale: 2,
-                        minScale: 0.5,
-                        scaleSpeed: 1.2,
-                    },
-                    media: MEDIA_PATH,
-                    trashcan: true,
-                }}
-                onWorkspaceChange={workspaceDidChange}
-                onXmlChange={setXml}
-            />
-            <pre className="prettyprint">
-                <code>{xml}</code>
-            </pre>
-            <pre className="prettyprint">
-                <code className="language-javascript">{javascriptCode}</code>
-            </pre>
+        <div className="app-container">
+            <div className="action-container">
+                <Upload accept=".zip" beforeUpload={loadProjectHandle}>
+                    <Button type="primary">load project</Button>
+                </Upload>
+                <Button type="primary" onClick={saveProjectClick}>
+                    save
+                </Button>
+            </div>
+            <Tabs defaultActiveKey="1" onChange={selectTabHandle}>
+                <TabPane tab="Blockly" key="1">
+                    <BlocklyWorkspace
+                        toolboxConfiguration={INITIAL_TOOLBOX_JSON}
+                        initialXml={INITIAL_XML}
+                        className="fill-height"
+                        workspaceConfiguration={{
+                            grid: {
+                                spacing: 20,
+                                length: 3,
+                                colour: "#ccc",
+                                snap: true,
+                            },
+                            zoom: {
+                                controls: true,
+                                wheel: true,
+                                startScale: 1.0,
+                                maxScale: 2,
+                                minScale: 0.5,
+                                scaleSpeed: 1.2,
+                            },
+                            media: MEDIA_PATH,
+                            trashcan: true,
+                        }}
+                        onWorkspaceChange={workspaceDidChange}
+                        onXmlChange={xmlChangeHandle}
+                        onInject={injectHandle}
+                    />
+                </TabPane>
+                <TabPane tab="JavaScript" key="2">
+                    <pre className="prettyprint">{javascriptCode}</pre>
+                </TabPane>
+                <TabPane tab="Xml" key="3">
+                    <pre className="prettyprint">{xml}</pre>
+                </TabPane>
+            </Tabs>
         </div>
     );
 }
---
+++ src/libs/helpers/project.ts
@@ -0,0 +1,49 @@
+import JSZip from "jszip";
+
+export const downloadFile = (path: string) => {
+    const linkInstance = document.createElement("a");
+    // 去除http前缀
+    path = path.replace(/^http(s){0,1}:/, "");
+    linkInstance.setAttribute("href", path);
+    linkInstance.style.display = "none";
+    document.body.appendChild(linkInstance);
+    linkInstance.click();
+    setTimeout(() => {
+        linkInstance.remove();
+    }, 300);
+};
+
+export const downloadFileByContext = (content: Blob, fileName: string) => {
+    const linkInstance = document.createElement("a");
+    const url = window.URL.createObjectURL(content);
+    linkInstance.setAttribute("href", url);
+    linkInstance.setAttribute("download", fileName);
+    linkInstance.style.display = "none";
+    document.body.appendChild(linkInstance);
+    linkInstance.click();
+    setTimeout(() => {
+        linkInstance.remove();
+    }, 300);
+};
+
+export const savaProject = (xmlCode: string, javascriptCode: string) => {
+    console.log("save", xmlCode, javascriptCode);
+    const zip = new JSZip();
+    zip.file("project.xml", xmlCode);
+    zip.file("project.js", javascriptCode);
+    zip.generateAsync({ type: "blob" }).then((content: Blob) => {
+        downloadFileByContext(content, "project.zip");
+    });
+};
+
+export const loadProjectXmlAsync = async (content: Blob) => {
+    try {
+        const zip = new JSZip();
+        const data = await zip.loadAsync(content);
+        const xmlFile = data.files["project.xml"];
+        const xmlStr = await xmlFile.async("text");
+        return xmlStr;
+    } catch (err) {
+        throw Error("加载失败");
+    }
+};
